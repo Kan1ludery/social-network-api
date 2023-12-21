8f54f48ef380f5a0750b68a82a564fa1e6bea67d
danil <danil.scheglov@mail.ru>
2023-12-21 21:38:18 +0300

    final_strokes

--- src/utils/csrfToken.js
+++ src/utils/csrfToken.js
@@ -1,11 +1,15 @@
 const csrf = require('csurf');
 
 // Создаем CSRF-токен и устанавливаем его в куки с httpOnly
-const csrfProtection = csrf({ cookie: {
-        httpOnly: true,
-        length: 48,
-        secure: true,
-        domain: 'yomessage-api.ru'} }
+const csrfProtection = csrf({
+        cookie: {
+            httpOnly: true,
+            length: 48,
+            secure: true,
+            domain: 'yomessage-api.ru',
+            sameSite: 'None', // Установите SameSite=None для разрешения передачи через разные сайты
+        }
+    }
 );
 
-module.exports = { csrfProtection };
+module.exports = {csrfProtection};
